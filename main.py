0bb2c72a83aca500bdb7589df6c4f71e9a9ce7a9
Nata <webmasternatalia777@gmail.com>
2023-07-17 18:01:05 +0300

    Added some classes

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 #import another_module
 #print(another_module.another_variable)
 
-#from turtle import Turtle,Screen
-#timmy = Turtle()
-#print(timmy)
-#timmy.shape("turtle")
-#timmy.color("coral")
-#timmy.forward(200)
+from turtle import Turtle,Screen
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("coral")
+timmy.forward(300)
 
 #my_screen = Screen()
 #print(my_screen.canvheight)
@@ -20,3 +20,15 @@ table.add_column("Pokemon Name",["Pickachu","Squirtle","Charmander"])
 table.add_column("Type",["Electric","Water","Fire"])
 table.align = "l"
 print(table)
+
+class User:
+    def __init__(self,user_id,username):
+        self.id = user_id
+        self.username = username
+        print("The user  being created")
+
+
+user_1 = User("001", "nata")
+user_2 = User("002", "jack")
+
+print(user_2.username)
