46f21aa0dcda41792bf003763eb9e75f09aaae32
Nata <webmasternatalia777@gmail.com>
2023-07-17 18:15:54 +0300

    Added some classes

--- main.py
+++ main.py
@@ -8,16 +8,11 @@ timmy.shape("turtle")
 timmy.color("coral")
 timmy.forward(300)
 
-#my_screen = Screen()
-#print(my_screen.canvheight)
-#print(my_screen.canvwidth)
-#my_screen.exitonclick()
-
 from prettytable import PrettyTable
 
 table = PrettyTable()
-table.add_column("Pokemon Name",["Pickachu","Squirtle","Charmander"])
-table.add_column("Type",["Electric","Water","Fire"])
+table.add_column("Pokemon Name", ["Pickachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
 table.align = "l"
 print(table)
 
